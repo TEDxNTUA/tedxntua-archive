46ad266fd802972e825800115cf09a6ad454f865
dinospapargyriou <dinospapargyriou@gmail.com>
2024-01-21 15:59:07 +0200

    Dinos Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,5 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/watch">Watch</Link>
-      </li>
-      <li>
-        <Link href="/blog">Blog Post</Link>
-      </li>
-      <li>
-        <Link href="/about">About Us</Link>
-      </li>
-    </ul>
-  );
+  return <div>TedxNTUA</div>;
 }
