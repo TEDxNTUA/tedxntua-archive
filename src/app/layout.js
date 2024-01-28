711bcf7f091a9f849abca0c99317d93ec4366384
john-papani <johnpapani1@gmail.com>
2024-01-28 19:59:15 +0200

    Change font to Helvetica

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black">
+      <body className="bg-black font-[Helvetica]">
         <Navbar />
         <div className="mt-[5rem]"></div>
         {children}
