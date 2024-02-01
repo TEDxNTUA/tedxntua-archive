339d4614d79d3ea61e65422926ade69d3687d5d6
john-papani <johnpapani1@gmail.com>
2024-02-01 22:35:15 +0200

    Add footer to all pages

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import { BLOGPOSTS } from "../../../data/blogposts";
 
 import BlogGrid from "@/components/Blog/BlogGrid";
@@ -11,6 +12,7 @@ function BlogPage() {
         team!
       </h1>
       <BlogGrid blogposts={BLOGPOSTS} />
+      <Footer />
     </div>
   );
 }
