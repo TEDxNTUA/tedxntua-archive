52670de7ce99feec5acf606767a79aab7be576e5
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2024-02-02 05:01:34 +0200

    new blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,8 +4,15 @@ import BlogGrid from "@/components/Blog/BlogGrid";
 
 function BlogPage() {
   return (
-    <div className="bg-blue-100">
-      <h1 className="text-white w-[75%] text-3xl lg:text-7xl text-center font-bold italic mx-auto py-16">
+    <div
+      className="bg-neutral-800"
+      // style={{
+      //   // backgroundImage: `url('/about/about4a.jpeg')`,
+      //   backgroundImage: `url('BG.png.jpeg')`,
+      //   backgroundSize: "cover", // Optional: adjust as needed
+      // }}
+    >
+      <h1 className="text-white w-[75%] text-4xl lg:text-7xl text-left font-bold  py-16 md:w-[80%] mx-auto">
         <span className="text-[#eb0028]">TEDx</span>NTUA Organisers' Blog
       </h1>
       <BlogGrid blogposts={BLOGPOSTS} />
