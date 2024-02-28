b987d9a530c069989c48ff3744f107bc05706918
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2024-02-28 20:59:45 +0200

    add bg to footer

--- src/components/Footer/NewFooter.js
+++ src/components/Footer/NewFooter.js
@@ -4,7 +4,7 @@ import SocialMediaFooter from "../NavBar/SocialMediaFooter";
 
 function NewFooter() {
   return (
-    <footer className="c-footer relative overflow-hidden pt-[2rem] sm:pt-[1rem] md:pt-[10rem] z-0">
+    <footer className="c-footer relative overflow-hidden pt-[2rem] sm:pt-[1rem] md:pt-[10rem] z-0 bg-our-black">
       <div className="c-footer-animation w-embed h-[100vh] object-cover md:object-none md:height-[50rem] absolute top-0 right-0 left-0 bottom-0">
         <svg
           className="overflow-hidden z-0"
