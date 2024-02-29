b91c2418bd45711f12ff8f43e70595ea34615b46
Giannis Tsagkaropoulos <98717569+GiannisTsagkaropoulos@users.noreply.github.com>
2024-02-29 23:23:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
+
+  // Optional: Change the output directory `out` -> `dist`
+   distDir: 'dist',
 };
 
 export default nextConfig;
