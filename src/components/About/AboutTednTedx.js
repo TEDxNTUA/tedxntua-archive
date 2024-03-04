f3a6c857da8cf209474da98450923f1f1582fcb4
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2024-03-04 04:34:56 +0200

    add gap to about page

--- src/components/About/AboutTednTedx.js
+++ src/components/About/AboutTednTedx.js
@@ -8,7 +8,7 @@ function AboutTednTedx() {
         backgroundSize: "cover", // Optional: adjust as needed
       }}
     >
-      <div className="text-white flex flex-col lg:flex-row lg:justify-between gap-[4rem] lg:gap-[1rem] text-justify lg:text-left lg:w-[80%] mx-auto">
+      <div className="text-white flex flex-col lg:flex-row lg:justify-between gap-[4rem] text-justify lg:text-left lg:w-[80%] mx-auto">
         <div className="left-col lg:width-[54%] w-[80%] lg:max-w-[50rem] mx-auto">
           <div className="title-wrapper mb-[2rem] text-[1rem] leading-[1.875rem]">
             <h2 className="text-[2.25rem] leading-[2.25rem] lg:text-[4.25rem] lg:leading-[4.25rem] 4xl:text-[6.25rem] 4xl:leading-[6.25rem] font-bold">
