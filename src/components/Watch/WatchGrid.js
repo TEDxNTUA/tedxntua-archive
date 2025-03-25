674ee2217fcc946324ed13d43c36babe9444923c
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2025-03-25 23:34:57 +0200

    feat(watchpage): new watchpage UI and  watchtalk filtering logic

--- src/components/Watch/WatchGrid.js
+++ src/components/Watch/WatchGrid.js
@@ -2,14 +2,14 @@ import WatchCard from "./WatchCard";
 
 export default function WatchGrid({talks}) {
   return (
-    <ul
-      className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 list-none gap-[6vh] lg:gap-10 w-10/12 mx-auto z-1 rounded-md lg:p-10 lg:bg-zinc-950`}
-    >
-      {talks.map(talk => (
-        <li key={talk.id}>
-          <WatchCard talk={talk} />
-        </li>
-      ))}
-    </ul>
+    <div className="max-w-[1600px] w-10/12 mx-auto z-1 rounded-md xl:bg-zinc-950 xl:p-10">
+      <ul className="grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 list-none gap-[6vh] xl:gap-10">
+        {talks.map(talk => (
+          <li key={talk.id}>
+            <WatchCard talk={talk} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
